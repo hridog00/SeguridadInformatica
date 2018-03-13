239f0d1333cea7b7b77768ea525bcfaf5f65d96b
hridog00 <hridog00@estudiantes.unileon.es>
2018-03-13 13:38:19 +0100

    practica 03

---
+++ practica3.py
@@ -0,0 +1,104 @@
+import math;
+import operator;
+
+def duplicar(f):
+    res =[]
+
+    for i in f:
+        for x in f:
+
+            res.append(i*x)
+
+
+    return res
+
+
+#ejercicio 1
+f = [83, 69, 67, 82, 69, 84, 79, 32, 68, 69, 32, 85, 78, 79, 32, 83, 69, 67, 82, 69, 84, 79, 32, 83, 69, 71, 85, 82, 79]
+total = 0.0
+
+
+f=[5, 1]
+f= duplicar(f)
+for elem in f:
+    total = total + elem
+p={}
+c={}
+print total
+letras = 'abcdefghijklmnopqrstuvwxyzABC'
+simbolo = ''
+x = 0
+for elem in f:
+    if x < 10:
+        simbolo = 'a00' + str(x)
+    else:
+        if x < 100:
+            simbolo = 'a0' + str(x)
+        else:
+            simbolo = 'a' + str(x)
+
+    p[simbolo] = elem/total;
+    c[simbolo] = ''
+    x = x +1
+
+#hallar entropia
+entropia = 0
+
+for x in p:
+    entropia= entropia + (p[x] * math.log((1/p[x]),2))
+
+print 'ENTROPIA'
+print entropia
+
+#huffman
+
+inicial = p.copy()
+print 'FRECUENCIAS INICIALES'
+print p
+
+while len(p)>1:
+
+    #cogemos las menosres probabilidades
+
+    aux = sorted(p.items(), key=operator.itemgetter(1), reverse=False)
+
+    e1 = aux[0][0];
+    e2 = aux[1][0];
+
+    pos = 0
+    while pos < len(e1):
+        sim = e1[pos:pos+4]
+
+        c[sim] = c[sim] + '0'
+        pos = pos +4
+    pos = 0
+    while pos <len(e2):
+        sim = e2[pos:pos + 4]
+        c[sim] = c[sim] + '1'
+        pos = pos + 4
+
+
+
+
+    p[e1 + e2] = p[e1]+p[e2];
+    p.pop(e1);
+    p.pop(e2);
+
+print 'RESULTADO FINAL'
+print(c)
+
+print 'LONGITUD MEDIA'
+long = 0;
+for elem in inicial:
+    long = long + inicial[elem] * len(c[elem])
+
+print long
+
+print 'EFICACIA'
+
+eficacia = entropia/(math.log(2,2)*long)
+print eficacia
+
+
+
+
--- prueba.py
+++ prueba.py
@@ -1,4 +1,14 @@
-letra = 'a'
-for x in range(0, 5):
-    letra = letra +1
-    print x
+len = ''
+
+variable = 0
+for x in range(0, 120):
+    if x<10:
+         len = 'a00' + str(x)
+    else:
+        if x < 100:
+            len = 'a0' + str(x)
+        else:
+            len = 'a'+str(x)
+    print len
+
+print len[:2]
